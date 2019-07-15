50d20ec3a33add89f33498ded0d788c8041e27da
wufly01@163.com <wufly01@163.com>
2019-07-15 16:09:34 +0800

    菜单项列表提交

--- src/main/java/com/allinpay/controller/MyErrorController.java
+++ src/main/java/com/allinpay/controller/MyErrorController.java
@@ -1,21 +1,21 @@
-package com.allinpay.controller;
-
-import org.springframework.boot.autoconfigure.web.ErrorController;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.http.HttpServletResponse;
-
-@Controller
-public class MyErrorController implements ErrorController {
-    @RequestMapping("/error")
-    public String dealAbNormal(HttpServletResponse response) {
-        System.out.println(response.getStatus());
-        return "/common/404";
-    }
-
-    @Override
-    public String getErrorPath() {
-        return "/error";
-    }
-}
+//package com.allinpay.controller;
+//
+//import org.springframework.boot.autoconfigure.web.ErrorController;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//import javax.servlet.http.HttpServletResponse;
+//
+//@Controller
+//public class MyErrorController implements ErrorController {
+//    @RequestMapping("/error")
+//    public String dealAbNormal(HttpServletResponse response) {
+//        System.out.println(response.getStatus());
+//        return "/common/404";
+//    }
+//
+//    @Override
+//    public String getErrorPath() {
+//        return "/error";
+//    }
+//}
--- src/main/java/com/allinpay/core/interceptor/AuthorityInterceptor.java
+++ src/main/java/com/allinpay/core/interceptor/AuthorityInterceptor.java
@@ -1,55 +1,55 @@
-package com.allinpay.core.interceptor;
-
-import lombok.extern.slf4j.Slf4j;
-import org.apache.shiro.SecurityUtils;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-@Component
-@Slf4j
-/**
- * @description: 主要针对于url的拦截，session超时处理，权限控制
- * @author: tanguang
- * @date: 2019-07-09
- */
-public class AuthorityInterceptor implements HandlerInterceptor {
-    private static String LOGIN = "/etc/login";
-    private static String RANDOM_CODE = "/etc/captcha";
-
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object o) throws Exception {
-        String url = request.getRequestURI();
-        //登录、获取验证码直接放行
-        if (LOGIN.equals(url) || RANDOM_CODE.equals(url)) {
-            return true;
-        }
-
-        //ajax请求 session超时处理
-        if (!SecurityUtils.getSubject().isAuthenticated()) {
-            if (null != request.getHeader("X-Requested-With") && request.getHeader("X-Requested-With").equalsIgnoreCase("XMLHttpRequest")) {
-                //在ajax响应头部设置一个sessionstatus状态，用于在ajax全局js(ajaxcommon.js)中判断
-                response.setHeader("sessionstatus", "timeout");
-                response.getWriter().print("timeout");
-                return false;
-            }
-        }
-
-        //做权限拦截
-
-        return true;
-    }
-
-    @Override
-    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
-
-    }
-
-    @Override
-    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
-
-    }
-}
+//package com.allinpay.core.interceptor;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.apache.shiro.SecurityUtils;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.servlet.HandlerInterceptor;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//@Component
+//@Slf4j
+///**
+// * @description: 主要针对于url的拦截，session超时处理，权限控制
+// * @author: tanguang
+// * @date: 2019-07-09
+// */
+//public class AuthorityInterceptor implements HandlerInterceptor {
+//    private static String LOGIN = "/etc/login";
+//    private static String RANDOM_CODE = "/etc/captcha";
+//
+//    @Override
+//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object o) throws Exception {
+//        String url = request.getRequestURI();
+//        //登录、获取验证码直接放行
+//        if (LOGIN.equals(url) || RANDOM_CODE.equals(url)) {
+//            return true;
+//        }
+//
+//        //ajax请求 session超时处理
+//        if (!SecurityUtils.getSubject().isAuthenticated()) {
+//            if (null != request.getHeader("X-Requested-With") && request.getHeader("X-Requested-With").equalsIgnoreCase("XMLHttpRequest")) {
+//                //在ajax响应头部设置一个sessionstatus状态，用于在ajax全局js(ajaxcommon.js)中判断
+//                response.setHeader("sessionstatus", "timeout");
+//                response.getWriter().print("timeout");
+//                return false;
+//            }
+//        }
+//
+//        //做权限拦截
+//
+//        return true;
+//    }
+//
+//    @Override
+//    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+//
+//    }
+//
+//    @Override
+//    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+//
+//    }
+//}
