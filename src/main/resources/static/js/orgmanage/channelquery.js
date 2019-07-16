ca784e9f058496872c81dfcb9eafc712bdd4cd3f
ChlorisVirgata <2673650213@qq.com>
2019-07-16 19:23:28 +0800

    1、机构查询照片放大增加；

--- src/main/resources/static/js/orgmanage/channelquery.js
+++ src/main/resources/static/js/orgmanage/channelquery.js
@@ -98,9 +98,11 @@ layui.use(['table', 'element', 'laypage', 'layer', 'form'], function () {
                 {field: 'contactPhone',  title: '联系人电话', hide: true},
                 {field: 'url',  title: '请求服务地址', hide: true},
 
+                {field: 'license',  title: '身份证证明', hide: true},
                 {field: 'idFront',  title: '身份证证明', hide: true},
                 {field: 'idBack',  title: '身份证反面', hide: true},
                 {field: 'agreement',  title: '协议图片', hide: true},
+
 
                 {field: 'parentId',  title: '父机构编号'},
                 {field: 'businessLicenceNo',  title: '营业执照编号'},
@@ -235,8 +237,53 @@ layui.use(['table', 'element', 'laypage', 'layer', 'form'], function () {
     form.on('submit(addFilter)', function (data) {
         return false;
     });
-});
 
+    /*图片点击事件*/
+    $(".pimg").click(function(){
+        var _this = $(this);//将当前的pimg元素作为_this传入函数
+        imgShow("#outerdiv", "#innerdiv", "#bigimg", _this);
+    });
+
+    function imgShow(outerdiv, innerdiv, bigimg, _this){
+        var src = _this.attr("src");//获取当前点击的pimg元素中的src属性
+        $("#bigimg").attr("src", src);//设置#bigimg元素的src属性
+
+        /*获取当前点击图片的真实大小，并显示弹出层及大图*/
+        $("<img/>").attr("src", src).load(function(){
+            var windowW = $(window).width();//获取当前窗口宽度
+            var windowH = $(window).height();//获取当前窗口高度
+            var realWidth = this.width;//获取图片真实宽度
+            var realHeight = this.height;//获取图片真实高度
+            var imgWidth, imgHeight;
+            var scale = 0.8;//缩放尺寸，当图片真实宽度和高度大于窗口宽度和高度时进行缩放
+
+            if(realHeight>windowH*scale) {//判断图片高度
+                imgHeight = windowH*scale;//如大于窗口高度，图片高度进行缩放
+                imgWidth = imgHeight/realHeight*realWidth;//等比例缩放宽度
+                if(imgWidth>windowW*scale) {//如宽度扔大于窗口宽度
+                    imgWidth = windowW*scale;//再对宽度进行缩放
+                }
+            } else if(realWidth>windowW*scale) {//如图片高度合适，判断图片宽度
+                imgWidth = windowW*scale;//如大于窗口宽度，图片宽度进行缩放
+                imgHeight = imgWidth/realWidth*realHeight;//等比例缩放高度
+            } else {//如果图片真实高度和宽度都符合要求，高宽不变
+                imgWidth = realWidth;
+                imgHeight = realHeight;
+            }
+            $(bigimg).css("width",imgWidth);//以最终的宽度对图片缩放
+
+            var w = (windowW-imgWidth)/2;//计算图片与窗口左边距
+            var h = (windowH-imgHeight)/2;//计算图片与窗口上边距
+            $(innerdiv).css({"top":h, "left":w});//设置#innerdiv的top和left属性
+            $(outerdiv).fadeIn("fast");//淡入显示#outerdiv及.pimg
+        });
+
+        $(outerdiv).click(function(){//再次点击淡出消失弹出层
+            $(this).fadeOut("fast");
+        });
+    }
+
+});
 
 function query(){
     var index = layer.alert("立即提交", function () {
